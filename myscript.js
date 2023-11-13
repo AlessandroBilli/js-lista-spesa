aa052c1da00dc0be9a92f0f558ca9bf84e99475b
AlessandroBilli <skillmanbungo@gmail.com>
2023-11-13 17:48:31 +0100

    first commit

---
+++ myscript.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const listaSpesa = [
+    "Pane",
+    "Pollo",
+    "banana",
+    "Broccoli",
+    "Riso",
+    "Uova",
+    "Latte",
+    "Fagioli",
+    "Tonno",
+    "Pasta"
+];
+
+let indice = 0;
+
+while (indice < listaSpesa.length) {
+    console.log(listaSpesa[indice]);
+    indice += 1;
+}
